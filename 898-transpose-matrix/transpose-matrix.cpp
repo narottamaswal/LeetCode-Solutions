bae804db454ff5c141e6477d7fc16ad6333035c5
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2023-12-10 22:30:04 +0530

    Time: 9 ms (63.33%) | Memory: 10.9 MB (45.29%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.cpp
+++ 898-transpose-matrix/transpose-matrix.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
-        int rows = matrix.size();
-        int cols = matrix[0].size();
-        vector<vector<int>> ans(cols,vector<int>(rows,0));
-        
-        // Insert In Transpose Matrix
-        for (int i = 0; i < rows; i++)
-            for(int j = 0; j < cols; j++)
+        vector<vector<int>> ans(matrix[0].size(),vector<int>(matrix.size(),0));
+
+        for (int i = 0; i < matrix.size(); i++)
+            for(int j = 0; j < matrix[i].size(); j++)
                 ans[j][i] = matrix[i][j];
         return ans;
     }
