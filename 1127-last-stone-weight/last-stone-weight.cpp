edf6143ff620d1cc4e044b85dda2920475c16b0b
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2023-12-14 22:24:41 +0530

    Time: 4 ms (18.68%) | Memory: 8 MB (63.39%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.cpp
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -5,12 +5,14 @@ public:
         for(auto it:stones){
             pq.push(it);
         }
-        while(pq.size()>=2){
+        while(pq.size()>1){
             int top1 = pq.top();
             pq.pop();
             int top2 = pq.top();
             pq.pop();
-            if(top1!=top2){
+            if(top1==top2){
+                continue;
+            }else{
                 pq.push(top1-top2);
             }
         }
