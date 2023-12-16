26b1ebb455e2cb3c17cbeb8657f2b0f5572b5e78
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2023-12-16 17:19:21 +0530

    Time: 295 ms (36.05%) | Memory: 116.1 MB (41.47%) - LeetSync

--- 1876-map-of-highest-peak/map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // basic setup
     int delX[4]={0,1,0,-1};
     int delY[4]={1,0,-1,0};
     bool safe(int i,int j,int n,int m){
@@ -8,13 +9,15 @@ public:
     vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
         int n = isWater.size();
         int m = isWater[0].size();
-        queue<pair<pair<int,int>,int>> q;
+        queue<pair<int,int>> q;
         vector<vector<int>> vis(n,vector<int>(m,-1));
         vector<vector<int>> ans(n,vector<int>(m,-1));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(isWater[i][j]==1){
-                    q.push({{i,j},-1});
+                    // insert the parent also where the node is coming from during bfs in all directions during the traversal
+                    // for initial 1 (water) we'll add -1;
+                    q.push({i,j});
                     vis[i][j]=-1;
                     ans[i][j]=0;
                 }
@@ -22,16 +25,19 @@ public:
         }
         while(!q.empty()){
             auto it = q.front();
-            int x = it.first.first;
-            int y = it.first.second;
-            int parent = it.second;
+            int x = it.first;
+            int y = it.second;
             q.pop();
             for(int i=0;i<4;i++){
                 int newX = x+delX[i];
                 int newY = y+delY[i];
                 if(safe(newX,newY,n,m) && isWater[newX][newY]==0){
+                    // if new element is -1 that means its not yet visitied any number of times so we'll insert this in queue
+                    // with parent as 0 and vis as 0 and increment the distance by newpoint = current point value +1
+
+                    // if an element is coming for the second time in traversal then we see if its already covered.
                     if(vis[newX][newY]==-1){
-                        q.push({{newX,newY},0});
+                        q.push({newX,newY});
                         vis[newX][newY]=0;
                         ans[newX][newY]=ans[x][y]+1;
                     }
