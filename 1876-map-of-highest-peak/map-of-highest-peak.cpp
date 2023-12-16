0ddd31ab910dfca025a3795a4722ffbcac5cd8c5
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2023-12-16 17:20:57 +0530

    Time: 312 ms (28.29%) | Memory: 116 MB (43.02%) - LeetSync

--- 1876-map-of-highest-peak/map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -7,14 +7,12 @@ public:
         return i>=0 && i<n && j>=0 && j<m;
     }
     vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
-        int n = isWater.size();
-        int m = isWater[0].size();
+        int n = isWater.size(),m=isWater[0].size();
         queue<pair<int,int>> q;
-        vector<vector<int>> vis(n,vector<int>(m,-1));
-        vector<vector<int>> ans(n,vector<int>(m,-1));
+        vector<vector<int>> vis(n,vector<int>(m,-1)),ans(n,vector<int>(m,-1));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(isWater[i][j]==1){
+                if(isWater[i][j]){
                     // insert the parent also where the node is coming from during bfs in all directions during the traversal
                     // for initial 1 (water) we'll add -1;
                     q.push({i,j});
@@ -31,12 +29,12 @@ public:
             for(int i=0;i<4;i++){
                 int newX = x+delX[i];
                 int newY = y+delY[i];
-                if(safe(newX,newY,n,m) && isWater[newX][newY]==0){
+                if(safe(newX,newY,n,m) && !isWater[newX][newY]){
                     // if new element is -1 that means its not yet visitied any number of times so we'll insert this in queue
                     // with parent as 0 and vis as 0 and increment the distance by newpoint = current point value +1
 
                     // if an element is coming for the second time in traversal then we see if its already covered.
-                    if(vis[newX][newY]==-1){
+                    if(-1==vis[newX][newY]){
                         q.push({newX,newY});
                         vis[newX][newY]=0;
                         ans[newX][newY]=ans[x][y]+1;
