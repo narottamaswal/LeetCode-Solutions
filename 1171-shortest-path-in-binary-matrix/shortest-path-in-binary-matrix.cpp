4bf5a6fb705ee43ce160ac1c21cbe45a172f5e41
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2023-12-17 16:18:55 +0530

    Time: 65 ms (42.38%) | Memory: 21.6 MB (65.29%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
-    bool isValidCoord(int i,int j,int n,int m){
-        return i>=0 && j>=0 && i<n && j<m;
-    }
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m =grid[0].size();
+        int n=grid.size();
+        int m =grid.size();
+        auto safe=[&](int i,int j){
+            return i>=0 && j>=0 && i<n && j<m;
+        };
         if(grid[0][0]==1 || grid[n-1][m-1]==1)return -1;
         vector<vector<bool>> vis(n,vector<bool>(m,false));
-        queue<pair<int,int>> q;
-        q.push({0,0});
-        vis[0][0]=true;
+        queue<pair<pair<int,int>,int>> q;
+        q.push({{0,0},1});
         int coordX[]={0,0,1,-1,1,-1,-1,1};
         int coordY[]={1,-1,0,0,-1,1,-1,1};
-        int steps=0;
+        vis[0][0]=true;
+        bool flag=false;
+        int ans=INT_MAX;
         while(!q.empty()){
-            int size = q.size();
-            while(size--){
-                auto it=q.front();
-                q.pop();
-                int x = it.first;
-                int y= it.second;
-                if(x==n-1 && y==m-1){
-                    return steps+1;
-                }
-                for(int j=0;j<8;j++){
-                    int newX = x+coordX[j];
-                    int newY = y+coordY[j];
-                    if(isValidCoord(newX,newY,n,m) && grid[newX][newY]==0 && !vis[newX][newY]){
-                        vis[newX][newY]=true;
-                        q.push({newX,newY});
-                    }
+            int x = q.front().first.first;
+            int y = q.front().first.second;
+            int dis = q.front().second;
+            if(x==n-1 && y==m-1){
+                flag=true;
+                ans=min(ans,dis);
+            }
+            q.pop();
+            for(int i=0;i<8;i++){
+                int newX= x+coordX[i];
+                int newY= y+coordY[i];
+                if(safe(newX,newY) && !grid[newX][newY] && !vis[newX][newY]){
+                    vis[newX][newY]=true;
+                    q.push({{newX,newY},dis+1});
                 }
             }
-            steps++;
         }
-        return -1;
+        return flag?ans:-1;
     }
 };
