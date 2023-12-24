becc471e6157ab0688d518bafd42bbe48d141421
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2023-12-24 20:55:58 +0530

    Time: 39 ms (48.53%) | Memory: 32.9 MB (64.64%) - LeetSync

--- 1062-partition-array-into-three-parts-with-equal-sum/partition-array-into-three-parts-with-equal-sum.cpp
+++ 1062-partition-array-into-three-parts-with-equal-sum/partition-array-into-three-parts-with-equal-sum.cpp
@@ -2,21 +2,28 @@ class Solution {
 public:
     bool canThreePartsEqualSum(vector<int>& arr) {
         int prefixsum=0;
+        //calculate prefix sum
         for(auto it:arr){
             prefixsum+=it;
         }
-        if(prefixsum%3) return false;        
-        int onepairsum = prefixsum/3;
+        // if total sum is not divisible by 3 then 
+        // we cannot make 3 combinations out of it 
+        // so return false
+        if(prefixsum%3) return false;  
 
-        int currentSum=0;
-        int ans=0;
+        // we'll split the array based on sum      
+        int onepairsum = prefixsum/3;
+        int currentSum=0,ans=0;
         for(int i=0;i<arr.size();i++){
             currentSum+=arr[i];
+            // if current sum is equal to one pair sum 
+            // then increment the ans and reset the sum to 0
             if(currentSum==onepairsum){
                 currentSum=0;
                 ans++;
             }
         }
+        // there can be more than 3 combinations as well and that is valid thats why ans>=3
         return ans>=3;
     }
 };
