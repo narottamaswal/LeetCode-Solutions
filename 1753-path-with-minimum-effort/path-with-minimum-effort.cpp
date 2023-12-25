0301d6a0d56970bb7487b6237471ea48ab3e8ff2
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2023-12-26 00:02:44 +0530

    Time: 84 ms (99.18%) | Memory: 19.3 MB (95.32%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,36 +1,41 @@
 class Solution {
+
 public:
-    bool safe(int i, int j, int n, int m) {
-        return i >= 0 && j >= 0 && i < n && j < m;
+    bool safe(int i,int j,int n,int m){
+        return i>=0 && j>=0 && i<n && j<m;
     }
     int minimumEffortPath(vector<vector<int>>& heights) {
-        int n = heights.size(), m = heights[0].size();
-        queue<pair<int, int>> q;
-        int coordX[] = {1,0,-1,0};
-        int coordY[] = {0,1,0,-1};
-        q.push({0, 0});
-        vector<vector<int>> ans(n, vector<int>(m, INT_MAX));
+        int n = heights.size(),m= heights[0].size();
+        int coordX[]={0,1,0,-1};
+        int coordY[]={1,0,-1,0};
+        priority_queue<pair<int,pair<int,int>>> pq;
+        pq.push({0,{0,0}});
+        vector<vector<int>> ans(n,vector<int>(m,INT_MAX));
         ans[0][0]=0;
-        while (!q.empty()) {
-            auto it = q.front();
-            q.pop();
-            int currentX = it.first;
-            int currentY = it.second;
-            int currentCost = ans[currentX][currentY];
-            int currentHeight = heights[currentX][currentY];
-            for (int i = 0; i < 4; i++) {
-                int newX = currentX + coordX[i];
-                int newY = currentY + coordY[i];
-                if (safe(newX, newY, n, m)){
-                    int newHeight = heights[newX][newY];
-                    int newCost = max(currentCost,abs(currentHeight-newHeight));
-                    if(newCost<ans[newX][newY]){
-                        ans[newX][newY]=newCost;
-                        q.push({newX,newY});
+        while(!pq.empty()){
+            auto it = pq.top();
+            pq.pop();
+            int currentCost = -it.first;
+            int currentX = it.second.first;
+            int currentY = it.second.second;
+            if(currentCost>ans[currentX][currentY]){
+                continue;
+            }
+            if(currentX==n-1 && currentY==m-1){
+                return currentCost;
+            }
+            for(int i=0;i<4;i++){
+                int newX = currentX+coordX[i];
+                int newY = currentY+coordY[i];
+                if(safe(newX,newY,n,m)){
+                    int newEffort = max(ans[currentX][currentY],abs(heights[newX][newY]-heights[currentX][currentY]));
+                    if(newEffort<ans[newX][newY]){
+                        ans[newX][newY]=newEffort;
+                        pq.push({-newEffort,{newX,newY}});
                     }
                 }
             }
         }
-        return ans[n - 1][m - 1];
+        return ans[n-1][m-1];
     }
 };
