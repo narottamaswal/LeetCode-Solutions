b2da88f3e958202f12b1d4e704e1bec54d5fad32
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2024-01-07 15:24:04 +0530

    Time: 4 ms (25.48%) | Memory: 8.1 MB (68.87%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
-    int moneyRobbed(vector<int> &dp,int n,vector<int> & nums,int currentIndex){
-        if(currentIndex>n-1){
-            return 0;
-        }
-        if(dp[currentIndex]!=-1){
-            return dp[currentIndex];
-        }
-       dp[currentIndex]=max(
-            nums[currentIndex]+moneyRobbed(dp,n,nums,currentIndex+2),
-            nums[currentIndex]+moneyRobbed(dp,n,nums,currentIndex+3)
-        );
-        return dp[currentIndex];
-    }
     int rob(vector<int>& nums) {
         if(nums.size()==1)return nums[0];
         else if(nums.size()==2)return max(nums[0],nums[1]);
         vector<int> dp(105,-1);
-        int finalans=0;
-         return max(
-             moneyRobbed(dp,nums.size(),nums,0),
-             moneyRobbed(dp,nums.size(),nums,1)
-         );
+        int n = nums.size();
+        dp[n - 1] = nums[n - 1];
+        dp[n] = 0;  
+        for (int i = n - 2; i >= 0; i--) {
+            dp[i] = max(nums[i] + dp[i + 2], nums[i] + dp[i + 3]);
+        }
+        return max(dp[0], dp[1]);
     }
 };
