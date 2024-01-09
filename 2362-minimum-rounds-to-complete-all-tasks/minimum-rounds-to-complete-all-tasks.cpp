d267da2d0b0a940ea57515e040858534601fd0d2
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2024-01-10 00:17:33 +0530

    Time: 156 ms (68.88%) | Memory: 104 MB (67.20%) - LeetSync

--- 2362-minimum-rounds-to-complete-all-tasks/minimum-rounds-to-complete-all-tasks.cpp
+++ 2362-minimum-rounds-to-complete-all-tasks/minimum-rounds-to-complete-all-tasks.cpp
@@ -2,23 +2,14 @@ class Solution {
 public:
     int minimumRounds(vector<int>& tasks) {
         unordered_map<int,int> mp;
+        int ans=0,rem=0;
         for(auto it:tasks){
             mp[it]++;
         }
-        priority_queue<int,vector<int>,greater<int>> pq;
-        for(auto it:mp){
-            if(it.second==1){
-                return -1;
-            }
-            pq.push(it.second);
-        }
-        int ans=0;
-        while(!pq.empty()){
-            auto it = pq.top();
-            pq.pop();
-            int task_count = it;
+        for(auto element:mp){
+            int task_count =element.second;
+            if(task_count==1) return -1;
             while(task_count>0){
-                int rem;
                 if(task_count<3){
                      rem = task_count-2;
                 }else{
@@ -28,9 +19,7 @@ public:
                         rem = min(task_count-2,task_count-3);
                     }
                 }
-                if(rem==1){
-                    return -1;
-                }
+                if(rem==1)return -1;
                 ans++;
                 task_count=rem;
             }
