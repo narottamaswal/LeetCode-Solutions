7016a03d574b8ebffd5981c8ffce3317824be9d5
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2024-01-11 22:56:44 +0530

    Time: 67 ms (57.60%) | Memory: 48.5 MB (35.15%) - LeetSync

--- 3202-high-access-employees/high-access-employees.cpp
+++ 3202-high-access-employees/high-access-employees.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     vector<string> findHighAccessEmployees(vector<vector<string>>& access_times) {
-        unordered_map<string,vector<string>> mp;
+        unordered_map<string,vector<int>> mp;
         for(auto it:access_times){
-            string element = it[0];
-            string aa = it[1];
-            mp[element].push_back(aa);
+            int aa = stoi(it[1]);
+            mp[it[0]].push_back(aa);
         }
         vector<string> ans;
         for(auto it:mp){
             string element = it.first;
-            vector<string> str = it.second;
+            vector<int> &str = it.second;
             sort(str.begin(),str.end());
             int i=2;
             while(i<str.size()){
-                string aa = str[i];
-                string ab = str[i-2];
-                if(stoi(aa)-stoi(ab)<100){
+                if(str[i]- str[i-2]<100){
                     ans.push_back(element);
                     break;
                 }
