4fd06c3534fd91c4531d8359ff92912eccaaa9d0
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2024-01-17 18:43:01 +0530

    Time: 4 ms (39.60%) | Memory: 7.2 MB (99.98%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
+    void insert(int i,int num,vector<int>& nums,vector<vector<int>> &subsets_vector){
+        int count=0;
+        while(num!=0){
+            int res = num & 1;
+            if(res>0){
+                subsets_vector[i].push_back(nums[count]);
+            }
+            count++;
+            num =num>>1;
+        }
+    }
     vector<vector<int>> subsets(vector<int>& nums) {
         int n = nums.size();
-        int no_of_subsets = 1 << n;
-        vector<vector<int>> subsets_vector(no_of_subsets);
-        for(int i=0;i<no_of_subsets;i++){
-            for(int j=0;j<n;j++){
-                if((i>>j) & 1){
-                    subsets_vector[i].push_back(nums[j]);
-                }
-            }
+        int subset_length = 1<<n;
+        vector<vector<int>> subsets_vector(subset_length);
+        for(int i=0;i<subset_length;i++){
+            insert(i,i,nums,subsets_vector);
         }
         return subsets_vector;
     }
