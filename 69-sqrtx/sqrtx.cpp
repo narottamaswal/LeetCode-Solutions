d8a8baa825cbda01279cb25382714ecbdafbc3c1
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2024-01-21 15:16:17 +0530

    Time: 2 ms (52.07%) | Memory: 7.4 MB (16.28%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==1)return 1;
+        auto condition=[&](long long int a,long long int x1){
+            if(a*a==x1){
+                return 1;
+            }else if(a*a<x1){
+                return 2;
+            }else{
+                return 0;
+            }
+        };
+        long long int low=1, high= x,mid=0, ans=0;
+        while(low<=high){
+            mid = high-(high-low)/2;
+            long long int temp = condition(mid,x);
+            if(temp==1){
+                return mid;
+            }else if(temp==2){
+                ans=mid;
+                low = mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+};
