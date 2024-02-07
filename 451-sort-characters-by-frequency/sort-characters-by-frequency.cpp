b92e76eaf0264a1521cac15eefaee6c49d527d91
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2024-02-07 19:10:32 +0530

    Time: 14 ms (33.47%) | Memory: 10.9 MB (14.35%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        string ans;
+        unordered_map<char,int> mp;
+        for(auto it:s){
+            mp[it]++;
+        }
+        priority_queue<pair<int,char>> pq;
+        for(auto it:mp){
+            pq.push({it.second,it.first});
+        }
+        while(!pq.empty()){
+            for(int i=0;i<pq.top().first;i++){
+                ans+=pq.top().second;
+            }
+            pq.pop();
+        }
+        return ans;
+    }
+};
