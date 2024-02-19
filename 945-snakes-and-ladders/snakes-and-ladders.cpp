f9065b51d8eedcbfbc54a719c5c30f7fcf0b9ad6
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2024-02-19 20:57:02 +0530

    Time: 9 ms (91.49%) | Memory: 16.3 MB (29.86%) - LeetSync

---
+++ 945-snakes-and-ladders/snakes-and-ladders.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int snakesAndLadders(vector<vector<int>>& board) {
+        int rows = board.size(), target= rows*rows;
+        int count=1;
+        vector<pair<int,int>> cells(target+1);
+        vector<int> columns(rows);
+        iota(columns.begin(),columns.end(),0);
+        for(int row=rows-1;row>=0;row--){
+            for(auto col:columns){
+                cells[count++]={row,col};
+            }
+            reverse(columns.begin(),columns.end());
+        }
+        vector<int> distance(target+1,-1);
+        distance[1]=0;
+        queue<int> q;
+        q.push(1);
+        while(!q.empty()){
+            int curr = q.front();
+            q.pop();
+            for(int next = curr+1;next<=min(curr+6,target);next++){
+                auto [row,column]=cells[next];
+                int dest = board[row][column]!=-1?board[row][column]:next;
+                if(distance[dest]==-1){
+                    distance[dest]=distance[curr]+1;
+                    q.push(dest);
+                }
+            }
+        }
+        return distance[target];
+    }
+};
+
