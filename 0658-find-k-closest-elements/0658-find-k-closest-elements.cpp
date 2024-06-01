407f054db097720f3af48f97e77e17d651d1b190
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2024-06-01 16:12:54 +0530

    Time: 19 ms (96.31%), Space: 33.4 MB (99.81%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,36 +1,14 @@
-
 class Solution {
 public:
-// struct myComp {
-//     constexpr bool operator()(
-//         pair<int, int> const& a,
-//         pair<int, int> const& b)
-//         const noexcept
-//     {
-//         return a.second < b.second;
-//     }
-// };
-
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-
-            //priority_queue<pair<int, int>, vector<pair<int, int>>, myComp> pq;
- priority_queue<pair<int, int>> pq;
-        vector<int> ans(k,0);
-        int diff=0;
-        for(auto it:arr){
-            diff=abs(it-x);
-            pq.push({diff,it});
-            if(pq.size()>k){
-                pq.pop();
-            }
+    vector<int> findClosestElements(vector<int>& A, int k, int x) {
+        int left = 0, right = A.size() - k;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (x - A[mid] > A[mid + k] - x)
+                left = mid + 1;
+            else
+                right = mid;
         }
-        int i=k-1;
-        while(!pq.empty()){
-            ans[i]=pq.top().second;
-            pq.pop();
-            i--;
-        }
-        sort(ans.begin(),ans.end());
-        return ans;
+        return vector<int>(A.begin() + left, A.begin() + left + k);
     }
 };
