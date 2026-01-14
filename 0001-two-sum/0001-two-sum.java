3f981ac99d00bd0dafc741296d582abae0463164
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2026-01-14 15:43:29 +0530

    Time: 2 ms (99.13%), Space: 47.2 MB (26%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,15 +4,12 @@ class Solution {
         int[] ans = new int[2];
         Map<Integer,Integer> mp = new HashMap<>();
         for(int i=0;i<n;i++){
-            mp.putIfAbsent(nums[i],i);
-        }
-        for(int i=0;i<n;i++){
-            int k = Math.abs(nums[i]-target);
-            if(mp.containsKey(k) && mp.get(k)!=i){
-                ans[0]=mp.get(k);
+            if(mp.containsKey(target-nums[i])){
+                ans[0]=mp.get(target-nums[i]);
                 ans[1]=i;
                 return ans;
             }
+            mp.put(nums[i],i);
         }
         return ans;
     }
