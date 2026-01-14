d1a50dec05a360fa340cd20ed44225e45e230aee
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2026-01-14 14:49:55 +0530

    Time: 16 ms (45.71%), Space: 93.6 MB (34.99%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,10 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        int length = nums.length;
-        HashSet<Integer> se = new HashSet<>();
-        for(int i=0;i<length;i++){
+        Set<Integer> se = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(se.contains(nums[i])){
+                return true;
+            }
             se.add(nums[i]);
         }
-        return se.size()!=length;
+        return false;
     }
 }
