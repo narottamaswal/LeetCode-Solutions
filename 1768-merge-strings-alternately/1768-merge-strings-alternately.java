b1411e0af049d20cdfc3f30e817c543158a8baf0
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2026-01-15 22:15:36 +0530

    Time: 1 ms (95.19%), Space: 43.2 MB (51%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -5,11 +5,10 @@ class Solution {
         while(i<n && j<m){
             sb.append(word1.charAt(i++)).append(word2.charAt(j++));
         }
-        while(i<n){
-            sb.append(word1.charAt(i++));
-        }
-        while(j<m){
-            sb.append(word2.charAt(j++));
+        if(n>m){
+            sb.append(word1.substring(i));
+        }else{
+            sb.append(word2.substring(j));
         }
         return sb.toString();
     }
