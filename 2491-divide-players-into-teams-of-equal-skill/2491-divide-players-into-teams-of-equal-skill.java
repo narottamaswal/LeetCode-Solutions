3cd6e8b4b8b73d1c00f1d713a7190f83efde6455
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2026-01-18 01:54:41 +0530

    Time: 37 ms (5.13%), Space: 75.5 MB (90.8%) - LeetHub

---
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.java
@@ -0,0 +1,40 @@
+class Solution {
+    public long dividePlayers(int[] skill) {
+        long n = skill.length, teams=n/2;
+        if(n%2!=0) return -1;
+        if(n==2){
+            return skill[0]*skill[1];
+        }
+        long sum=0;
+        Map<Integer,Integer> mp = new HashMap<>();
+        for(int a:skill){
+            mp.put(a,mp.getOrDefault(a,0)+1);
+            sum+=a;
+        }
+        if(sum%teams!=0) return -1; 
+        int eachTeamSkill = Math.toIntExact(sum/teams) ;
+        sum=0;
+        for(int i=0;i<n;i++){
+            int c = skill[i];
+            int rem = Math.abs(eachTeamSkill-c);
+            if(mp.containsKey(rem)){
+                mp.put(c,mp.getOrDefault(c,0)-1);
+                mp.put(rem,mp.getOrDefault(rem,0)-1);
+                sum = sum+(c*rem);
+                if(mp.getOrDefault(c,0)<=0){
+                    mp.remove(c);
+                }
+                if(mp.getOrDefault(rem,0)<=0){
+                    mp.remove(rem);                    
+                }
+            }
+        }
+        System.out.println(mp.entrySet());
+        return mp.size()>0 ? -1 : sum;
+    }
+}
+
+
+// 2 4 3 3 1 5
+// 8 9 5
+// 5 
