4c8d271ce13d8a09c2c4af7f1c3a18d07976e205
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2026-01-18 00:53:29 +0530

    Time: 2 ms (28.74%), Space: 45.4 MB (69.83%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int compress(char[] chars) {
+        int n =chars.length;
+        if(n==1) return n;
+        int i=0,j=0;
+        StringBuilder str = new StringBuilder();
+        while(i<n-1){
+            int c=1;
+            while(i+1<n && chars[i]==chars[i+1]){
+                i++;
+                c++;
+            }
+            str.append(chars[i]);
+            if(c>1){
+                str.append(String.valueOf(c));
+            }
+            i++;
+        }
+        if(n>1 && chars[n-1]!=chars[n-2]){
+            str.append(chars[n-1]);
+        }
+        i=0;
+        j=0;
+        String s1 = str.toString();
+        while(i<chars.length && j<s1.length()){
+            chars[i++]=s1.charAt(j++);
+        }
+        return s1.length();
+    }
+}
