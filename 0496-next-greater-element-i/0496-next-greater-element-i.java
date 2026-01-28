a0e5139c54c0339d8c8fa59b8382b22d198193a2
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2026-01-29 02:08:49 +0530

    Time: 2 ms (99.33%), Space: 45.6 MB (8.58%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Deque<Integer> st = new ArrayDeque<>();
-        Map<Integer,Integer> mp = new HashMap<>();
         int[] ans = new int[nums1.length];
+        int[] freq = new int[10001];
         for(int i=nums2.length-1;i>=0;i--){
             while(!st.isEmpty() && st.peek()<nums2[i]){
                 st.pop();
             }
-            mp.put(nums2[i],st.isEmpty() ? -1 : st.peek());
+            freq[nums2[i]] =st.isEmpty() ? -1 : st.peek(); 
             st.push(nums2[i]);
         }
         for(int i=0;i<nums1.length;i++){
-            ans[i]=mp.getOrDefault(nums1[i],0);
+            ans[i]=freq[nums1[i]];
         }
         return ans;
         
