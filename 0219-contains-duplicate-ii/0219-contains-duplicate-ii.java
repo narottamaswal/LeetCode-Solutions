7e6e6a63933afe686ed5b3acebbeb1e782da6825
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2026-02-12 01:07:50 +0530

    Time: 44 ms (10.4%), Space: 90.9 MB (25.51%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        // sliding window
+        int n = nums.length;
+        int start = 0, end=0;
+        Map<Integer,Integer> mp = new HashMap<>();
+        while(end<n){
+            mp.put(nums[end],mp.getOrDefault(nums[end],0)+1);
+            if(mp.getOrDefault(nums[end],0)>1){
+                return true;
+            }
+            if(end>=k){
+                mp.put(nums[start],mp.getOrDefault(nums[start],0)-1);
+                start++;
+            }
+            end++;
+        }
+        return false;
+    }
+}
