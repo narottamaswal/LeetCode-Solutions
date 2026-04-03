0b5b22b6629296ffa0a3c545e155f161ef0ee0d1
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2026-04-03 12:52:46 +0530

    Time: 0 ms (100%), Space: 44.6 MB (97.04%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -14,29 +14,17 @@
  * }
  */
 class Solution {
-    public boolean hasPathSum(TreeNode root, int targetSum) {
+    private boolean sum(TreeNode root,int targetSum){
         if(root==null){
             return false;
         }
-        Queue<Pair<TreeNode,Integer>> q = new LinkedList<>();
-        q.offer(new Pair<>(root,0));
-        while(!q.isEmpty()){
-            int qSize = q.size();
-            for(int i=0;i<qSize;i++){
-                Pair<TreeNode,Integer> element = q.poll();
-                TreeNode node = (TreeNode) element.getKey();
-                Integer va = (Integer) element.getValue();
-                if(node.left==null && node.right==null){
-                    if(va+node.val==targetSum) return true;
-                }
-                if(node.left!=null){
-                    q.offer(new Pair<>(node.left,va+node.val));
-                }
-                if(node.right!=null){
-                    q.offer(new Pair<>(node.right,va+node.val));
-                }
-            }
+        targetSum-=root.val;
+        if(root.left==null && root.right==null && targetSum==0){
+            return true;
         }
-        return false;
+        return sum(root.left,targetSum) || sum(root.right,targetSum);
+    }
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        return sum(root,targetSum);
     }
 }
