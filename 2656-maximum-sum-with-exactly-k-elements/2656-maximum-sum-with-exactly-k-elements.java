af5fd47b322bacecb77857aaf44b36118281dcc0
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2026-04-04 20:19:48 +0530

    Time: 5 ms (45.81%), Space: 46.9 MB (70.73%) - LeetHub

--- 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.java
+++ 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.java
@@ -1,12 +1,12 @@
 class Solution {
     public int maximizeSum(int[] nums, int k) {
-        Arrays.sort(nums);
+        int max = Arrays.stream(nums).max().getAsInt();
         int ans = 0;
         int i=nums.length-1;
         while(k-- > 0 ){
-            int element = nums[i];
+            int element = max;
             ans+=element;
-            nums[i]=element+1;
+            max++;
         }
         return ans;
     }
