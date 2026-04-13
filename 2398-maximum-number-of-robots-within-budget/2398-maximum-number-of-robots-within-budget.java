130e47d87f1cb93713e5e0428222f523ecea259a
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2026-04-14 00:14:35 +0530

    Time: 42 ms (32.14%), Space: 66.1 MB (97.08%) - LeetHub

--- 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.java
+++ 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.java
@@ -1,32 +1,25 @@
-record Pair(int chargeTime,int index){};
 class Solution {
     public int maximumRobots(int[] chargeTimes, int[] runningCosts, long budget) {
-        int n = chargeTimes.length;
-        int ans=0;
-        int start = 0, end =0;
-        int[] prefixCost = new int[n];
-        prefixCost[0]=runningCosts[0];
-        for(int i=1;i<n;i++){
-            prefixCost[i]=prefixCost[i-1]+runningCosts[i];
-        }
+        int ans=0,start = 0, end =0;
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->{
             return Integer.compare(b[0], a[0]);
         });
-        while(end<n){
+        long cost = 0;
+        while(end<chargeTimes.length){
+            cost+=runningCosts[end];
             pq.offer(new int[]{chargeTimes[end],end});
-            int window = end-start+1;
-            int cost = pq.peek()[0]+(window * prefixCost[end]);
-            if(cost<=budget){
-                ans = Math.max(ans,window);
+            if(pq.peek()[0]+((end-start+1) * cost)<=budget){
+                ans = Math.max(ans,end-start+1);
             }else{
-                while(!pq.isEmpty() && start>pq.peek()[1]){
-                    pq.remove(nums[start]);
-                    window = end-start+1;
-                    cost = pq.peek()[0]+(window * (prefixCost[end]-prefix[start]));
-                    if(cost<=budget){
-                        ans = Math.max(ans,window);
+                while(!pq.isEmpty() && pq.peek()[0]+((end-start+1) * cost)>budget){
+                    cost-=runningCosts[start];
+                    while(!pq.isEmpty() && pq.peek()[1]<=start){
+                        pq.poll();
                     }
                     start++;
+                }
+                if(!pq.isEmpty() && pq.peek()[0]+((end-start+1) * cost) <=budget){
+                    ans = Math.max(ans,end-start+1);
                 }
             }
             end++;
