9dfd798f9458f6cf6cafbdd6ace794fd21800c53
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2026-04-14 21:35:32 +0530

    Time: 10 ms (7.03%), Space: 45.1 MB (8.66%) - LeetHub

---
+++ 0967-numbers-with-same-consecutive-differences/0967-numbers-with-same-consecutive-differences.java
@@ -0,0 +1,33 @@
+class Solution {
+    Set<Integer> ele = new HashSet<>();
+
+    private void dfs(int n,int k,StringBuilder sb,char a){
+        if(sb.length()==n){
+            if(sb.charAt(0)!='0'){
+                ele.add(Integer.parseInt(sb.toString()));
+            }
+            return;
+        }
+        for(char i='0';i<='9';i++){
+            if(Math.abs(a-i)==k){
+                sb.append(i);
+                dfs(n,k,sb,i);
+                sb.deleteCharAt(sb.length()-1);
+            }
+        }
+    }
+    public int[] numsSameConsecDiff(int n, int k) {
+        for(char i='0';i<='9';i++){
+            dfs(n,k,new StringBuilder(),i);
+        }
+        System.out.println(ele);
+        int[] ans = ele.stream().mapToInt(i -> i).toArray();
+        // retun ans;
+        // int[] ans = ele.stream().toArray(Integer[]::new);;
+        return ans;
+        // return new int[]{};
+    }
+}
+// n= 5
+// k = 4
+// 0000 to 9999
