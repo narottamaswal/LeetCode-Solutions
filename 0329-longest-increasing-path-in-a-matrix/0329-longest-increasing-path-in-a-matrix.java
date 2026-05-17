df3260e4529d2b1a582a801ea67ec424e3a15828
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2026-05-17 14:29:07 +0530

    Time: 8 ms (88.11%), Space: 47.2 MB (48.33%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -1,52 +1,38 @@
 class Solution {
+    int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    
     public int longestIncreasingPath(int[][] matrix) {
-        int row = matrix.length;
-        int col = matrix[0].length;
-        if (row == 1 && col == 1) {
-            return 1;
-        }
-
+        int row = matrix.length,col = matrix[0].length;
         int max = 0;
         int[][] memo = new int[row][col];
-
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 if (memo[i][j] == 0) {
-                    max = Math.max(max, dfs(matrix, memo, i, j));
+                    max = Math.max(max, dfs(row,col,matrix, memo, i, j));
                 }
             }
         }
-
         return max;
     }
-
-    public int dfs(int[][] matrix, int[][] memo, int x, int y) {
+    private boolean isValid(int rows,int cols,int x,int y){
+        return x>=0 && y>=0 && x<rows && y<cols;
+    }
+    public int dfs(int row,int col,int[][] matrix, int[][] memo, int x, int y) {
         if (memo[x][y] > 0) {
             return memo[x][y];
         }
-
-        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
         int ans = 1;
-
-        int row = matrix.length;
-        int col = matrix[0].length;
-
         for (int[] dir : dirs) {
             int nextX = x + dir[0];
             int nextY = y + dir[1];
-
-            if (nextX < 0 || nextX >= row || nextY < 0 || nextY >= col) {
+            if (!isValid(row,col,nextX,nextY)) {
                 continue;
             }
-
             if (matrix[nextX][nextY] <= matrix[x][y]) {
                 continue;
             }
-
-            ans = Math.max(ans, dfs(matrix, memo, nextX, nextY) + 1);
+            ans = Math.max(ans, dfs(row,col,matrix, memo, nextX, nextY) + 1);
         }
-
         memo[x][y] = ans;
         return ans;
     }
