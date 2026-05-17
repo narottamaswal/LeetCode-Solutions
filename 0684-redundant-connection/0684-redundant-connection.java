ab31aca97035af962f3e9e84387835bb02fed706
Narottam Aswal <65083220+narottamaswal@users.noreply.github.com>
2026-05-18 01:11:22 +0530

    Time: 1 ms (91.26%), Space: 44.8 MB (80.86%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -0,0 +1,45 @@
+class Solution {
+    int[] parent;
+    int[] rank;
+    public int find(int node){
+        if(parent[node]==node){
+            return node;
+        }
+        int nodeParent = find(parent[node]);
+        parent[node]=nodeParent;
+        return parent[node];
+    }
+    public void union(int x,int y){
+        int x_parent = find(x);
+        int y_parent = find(y);
+        if(rank[x_parent] > rank[y_parent]) {
+            parent[y_parent] = x_parent;
+        } else if(rank[x_parent] < rank[y_parent]) {
+            parent[x_parent] = y_parent;
+        } else {
+            parent[x_parent] = y_parent;
+            rank[y_parent]++;
+        }
+    }
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        parent = new int[n+1];
+        rank = new int[n+1];
+        for(int i=1;i<=n;i++){
+            parent[i]=i;
+            rank[i]=0;
+        }
+        int[] ans=new int[]{0,0};
+        for(int[] edge: edges){
+            int parentU = find(edge[0]);
+            int parentV = find(edge[1]);
+            if(parentU==parentV){
+                ans[0]=edge[0];
+                ans[1]=edge[1];
+            }else{
+                union(parentU,parentV);
+            }
+        }
+        return ans;
+    }
+}
